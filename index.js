f23b450e8faa164c7a7331d97e123ce76a0ae622
mlmar <marcloussm@gmail.com>
2021-02-13 18:13:36 -0800

    post build

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.use(test);
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
