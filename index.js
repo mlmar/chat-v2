4eb6b7dd342b5254b7766367780784bfee74c6ca
mlmar <marcloussm@gmail.com>
2021-02-13 18:39:57 -0800

    procfile

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.use(test);
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/build/index.html'))
 })
